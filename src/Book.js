b17c5bd2735ee8e3f5effa3b9dfa7e2150fa08c7
Michael <arrietem@gmail.com>
2018-10-07 11:07:01 -0400

    Updated Book.js with TKAM template

--- src/Book.js
+++ src/Book.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react'
 class Book extends Component {
 	render() {
 		return (
-
+			<li>
+				<div class="book">
+					<div class="book-top">
+						<div class="book-cover" style="width: 128px; height: 193px; background-image: url();"></div>
+						<div class="book-shelf-changer"></div>
+					<div>
+					<div class="book-title">To Kill a Mockingbird</div>
+					<div class="book-authors">Harper Lee</div>
+				</div>
+			</li>
 		)
 	}
 }
