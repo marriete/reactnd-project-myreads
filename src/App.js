51cf3e46746be70c9e0f0367836f2b0a5f5f8ef2
Michael <arrietem@gmail.com>
2018-10-10 15:48:22 -0400

    Intermediate update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import Book from './Book.js'
 
 class BooksApp extends React.Component {
   state = {
--- src/Book.js
+++ src/Book.js
@@ -6,9 +6,9 @@ class Book extends Component {
 			<li>
 				<div class="book">
 					<div class="book-top">
-						<div class="book-cover" style="width: 128px; height: 193px; background-image: url();"></div>
+						<div class="book-cover"></div>
 						<div class="book-shelf-changer"></div>
-					<div>
+					</div>
 					<div class="book-title">To Kill a Mockingbird</div>
 					<div class="book-authors">Harper Lee</div>
 				</div>
