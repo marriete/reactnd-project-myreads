78bae77b52a0cb51212ed951ce34b2e13574b795
Michael <arrietem@gmail.com>
2018-10-15 15:44:25 -0400

    Added BookList component

---
+++ src/BookList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import BookShelf from './BookShelf.js'
+
+class BookList extends Component {
+	render() {
+		return (
+
+		)
+	}
+}
+
+export default BookList
