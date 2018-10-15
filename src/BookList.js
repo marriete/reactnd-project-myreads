296b0dde170c6f61594919c207a095f87e882f7d
Michael <arrietem@gmail.com>
2018-10-15 16:48:02 -0400

    Added shelfTypes to BookList component

--- src/BookList.js
+++ src/BookList.js
@@ -3,8 +3,15 @@ import BookShelf from './BookShelf.js'
 
 class BookList extends Component {
 	render() {
+			const shelfTypes = [
+				{ type: "currentlyReading", title: "Currently Reading" },
+				{ type: "wantToRead", title: "Want To Read" },
+				{ type: "read", title: "Read"}
+			];
 		return (
+			<div className="list-books-content">
 
+			</div>
 		)
 	}
 }
