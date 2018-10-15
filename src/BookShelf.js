50c6f5f8475d1ba0fb908b5e4447057d07ab7687
Michael <arrietem@gmail.com>
2018-10-15 15:05:47 -0400

    Added BookShelf component and template code

---
+++ src/BookShelf.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class BookShelf extends Component {
+	render() {
+		<div>
+			<div className="bookshelf">
+				<h2 className="bookshelf-title">Currently Reading</h2>
+				<div className="bookshelf-books">
+					<ol className="books-grid">
+
+					</ol>
+				</div>
+			</div>
+		</div>
+	}
+}
+
+export default BookShelf
