781548e9514bd506f48a025b19b74ed25507e4eb
Michael <arrietem@gmail.com>
2018-10-14 21:39:29 -0400

    Created Book component

--- src/Book.js
+++ src/Book.js
@@ -4,13 +4,21 @@ class Book extends Component {
 	render() {
 		return (
 			<li>
-				<div class="book">
-					<div class="book-top">
-						<div class="book-cover"></div>
-						<div class="book-shelf-changer"></div>
+				<div className="book">
+					<div className="book-top">
+						<div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${this.props.url})` }}></div>
+						<div className="book-shelf-changer">
+							<select>
+	              <option value="move" disabled>Move to...</option>
+	              <option value="currentlyReading">Currently Reading</option>
+	              <option value="wantToRead">Want to Read</option>
+	              <option value="read">Read</option>
+	              <option value="none">None</option>
+							</select>
+						</div>
 					</div>
-					<div class="book-title">To Kill a Mockingbird</div>
-					<div class="book-authors">Harper Lee</div>
+					<div className="book-title">{this.props.name}</div>
+					<div className="book-authors">{this.props.authors}</div>
 				</div>
 			</li>
 		)
