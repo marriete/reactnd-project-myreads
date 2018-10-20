16a60c347658754fde9362ca335c546f11850b28
Michael <arrietem@gmail.com>
2018-10-20 14:52:09 -0400

    Fixed bug where Search page would crash when no cover image was available for one or more of the search results

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import ShelfChanger from './ShelfChanger.js'
+import NotFound from './imgs/NotFound.png'
 
 class Book extends Component {
 	render() {
+				let thumbnail = NotFound
+		if (typeof(this.props.book.imageLinks) != "undefined")
+			thumbnail = `${this.props.book.imageLinks.smallThumbnail}`
+
 		return (
 			<li>
 				<div className="book">
 					<div className="book-top">
-						<div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${this.props.book.imageLinks.smallThumbnail})` }}></div>
+						<div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${thumbnail})` }}></div>
 						<ShelfChanger book={this.props.book} books={this.props.books}  changeShelf={this.props.changeShelf} />
 					</div>
 					<div className="book-title">{this.props.book.title}</div>
