7b5af6de26786303b7c6aed375698714c2677c00
Michael <arrietem@gmail.com>
2018-10-19 20:41:27 -0400

    Installed regexp and sortby

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class AddBook extends Component {
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({ query: query.trim() })
+	}
+
 	render() {
 		return (
-			<div>Add Book!</div>
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
 		)
 	}
 }
