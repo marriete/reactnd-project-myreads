bc0b3ea8d14d2ab684d2a8c69244b975ca5dde35
Michael <arrietem@gmail.com>
2018-10-20 13:53:39 -0400

    Implemented Search page that shows top 20 results of searched query for books

--- src/AddBook.js
+++ src/AddBook.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book.js'
 
 class AddBook extends Component {
 	state = {
-		query: ''
+		query: '',
+		searchedBooks: []
 	}
 
 	updateQuery = (query) => {
 		this.setState({ query: query.trim() })
+		BooksAPI.search(query).then((searchedBooks) => {
+			if (searchedBooks.length > 0)
+				this.setState({searchedBooks})
+			else
+				this.setState({searchedBooks: []})
+			}).catch((error) => (
+				this.setState({searchedBooks: []})
+			))
 	}
 
 	render() {
@@ -18,19 +29,18 @@ class AddBook extends Component {
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+        	{this.state.searchedBooks.length > 0 && (
+          <ol className="books-grid">
+						{console.log(this.state.searchedBooks)}
+						{this.state.searchedBooks.map((book) => (
+							<Book book={book} />
+						))}
+          </ol>
+        	)}
         </div>
       </div>
 		)
